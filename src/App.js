7c0105e5b3c5afd162c0e25a38acb7d402ef2c09
HassanKhan123 <hk260280@gmail.com>
2025-08-11 17:12:11 +0200

    enabled analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import {Analytics} from "@vercel/analytics/react";
 import "./App.scss";
 import Main from "./containers/Main";
 
 function App() {
   return (
     <div>
+      <Analytics />
       <Main />
     </div>
   );
